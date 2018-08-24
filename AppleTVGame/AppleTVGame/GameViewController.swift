d08f7d4524fbad94917abb3b88da8abae72b88f5
Felipe Procaci Kestelman <fpkest@gmail.com>
2018-08-24 15:44:27 -0300

    Add do projeto

---
+++ AppleTVGame/AppleTVGame/GameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GameViewController.swift
+//  AppleTVGame
+//
+//  Created by Felipe Kestelman on 24/08/18.
+//  Copyright © 2018 Felipe Kestelman. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+}
